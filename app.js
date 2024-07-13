7303dd5fca18f3812f456bb94e62103ed7523eb5
Vijay Sharma <157729319+omvijaysharma@users.noreply.github.com>
2024-07-13 12:20:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 let qrimage = document.querySelector("#qrimage");
-let input = document.querySelector("#input");
-let downloadLink = document.querySelector("#downloadLink");
+let input = document.querySelector("#textinput");
+let downloadbutton = document.querySelector("#downloadbutton");
 
 function generateQR() {
     let qrUrl = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + encodeURIComponent(input.value);
     qrimage.src = qrUrl;
-    downloadLink.href = qrUrl;
-    downloadLink.setAttribute("download", "qr_code.png");
-    downloadLink.style.display = "block";
+    downloadbutton.href = qrUrl;
+    downloadbutton.setAttribute("download", "qr_code.png");
+    downloadbutton.style.display = "block";
 }
